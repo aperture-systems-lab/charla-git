bd613083fdb0d3fe307be7d4f19b9b2c677c0947
Jose Miguel García Vélez <josgarciave@unal.edu.co>
2026-09-20 15:43:43 -0500

    fix: correct return indentation in poisson_probability

--- practica/retos/josgarciave/poisson_distribution_probability_calculator.py
+++
@@ -1,8 +0,0 @@
-# Link del reto: https://www.deep-ml.com/problems/81
-
-import math 
-
-def poisson_probability(k, lam):
-	val = (math.exp(-lam) * lam**k) / math.factorial(k)
-	pass
-	return round(val,5)
